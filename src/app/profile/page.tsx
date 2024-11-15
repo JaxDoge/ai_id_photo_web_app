446eb51f0e545ed95d331396c2bd0a4987c848e4
aaalea7 <35973992+aaalea7@users.noreply.github.com>
2024-11-14 22:11:11 -0800

    Update NavigationBar

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,6 +6,7 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import "./profilePage.css";
+import NavigationBar from "../NavigationBar/navigation";
 
 export default function ProfilePage() {
     const router = useRouter();
@@ -27,18 +28,7 @@ export default function ProfilePage() {
     return (
         <div className="profileContainer">
             {/* Left Sidebar */}
-            <aside className="leftSidebar">
-                <div style={{ marginTop: 100 }}></div>
-                <button className="icon" onClick={() => router.push('/generator')}>
-                    <i className="fas fa-house" />
-                </button>
-                <button className="icon active" onClick={() => router.push('/profile')}>
-                    <i className="fa-solid fa-grip" />
-                </button>
-                <button className="icon" onClick={() => router.push('/history')}>
-                    <i className="fas fa-file-alt" />
-                </button>
-            </aside>
+            <NavigationBar />
 
             {/* Main Content */}
             <main className="mainContent">
